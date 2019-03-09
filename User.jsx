95c2cd4934ff200e73c5fc42855cfe44a3aa4823
wendydesilva <wendydesilva9@gmail.com>
2019-03-09 12:44:22 +0530

    adding files

---
+++ User.jsx
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+
+const User = props => {
+
+     const {user} = props;
+     return<tr>
+         <td>{user.id}</td>
+         <td>{user.name}</td>
+     </tr>
+};
+export default User;
